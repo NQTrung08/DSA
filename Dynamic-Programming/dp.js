652615d9fca544bc90008fa093437a32db9c8d6f
NQTrung08 <nqtrung0810@gmail.com>
2025-05-12 22:38:52 +0700

    🔼 bottom up with fib

--- Dynamic-Programming/dp.js
+++ Dynamic-Programming/dp.js
@@ -31,3 +31,17 @@ console.log(fibMemo(5))
 console.log(memo)
 
 // => O(2n-1)
+
+// bottom up: sử dụng mảng và tính toán từ dưới lên trên
+
+const fibBottomUp = (n) => {
+  let fib = [];
+  fib[0] = 0
+  fib[1] = 1
+  for(let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2]
+  }
+  return fib[n]
+}
+
+console.log(fibBottomUp(7))
